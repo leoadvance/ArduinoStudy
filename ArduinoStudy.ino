79da6ac5dae6ef80f1a9a94176a6fdb37e5570bd
leoadvance <leo_advance@163.com>
2016-09-10 10:35:53 +0800

    Timer1 学习 5Hz LED翻转

--- ArduinoStudy.ino
+++ ArduinoStudy.ino
@@ -1,10 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-}
+#include "TimerOne.h"
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("Hello world\r\n");
+void setup()
+{
+  int tickEvent;
+  
+  // 新建串口
+  Serial.begin(115200);
+
+  pinMode(13, OUTPUT);
+
+  // 初始化定时器 单位us 周期0.1s。 最大 8388.608mS
+  Timer1.initialize(100000); 
+
+  // 定时器1 回调函数
+  Timer1.attachInterrupt(Timer1_Callback);  
+  
+}
+void Timer1_Callback()
+{
+  // 翻转
+  digitalWrite(13, digitalRead(13) ^ 1);
+}
+void loop() 
+{
+  Serial.print("Hello!\r\n");
   delay(1000);
 }
